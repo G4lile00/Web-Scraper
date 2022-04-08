f18e7c84b32bf83d0b7e6587dac5718070258c98
G4lile00 <vitinhomarcelo21@gmail.com>
2022-04-08 17:05:53 -0300

    edited writes

--- JavaScript/Writer.js
+++ JavaScript/Writer.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 
 module.exports = function write(data) {
-  let stream = fs.createWriteStream("./Data/data.txt");
+  let stream = fs.createWriteStream("./Data/data.txt", { flags: "a" });
 
   let datas = data.join("\n");
 
   stream.once("open", function (fd) {
-    stream.write(datas);
+    stream.write("\n" + datas);
 
     stream.end();
   });
